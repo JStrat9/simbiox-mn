3fd22a5345017072d8821ad7bc06cda33d3e1366
joanl <joanlapaix.d@gmail.com>
2025-12-21 10:09:56 +0100

    fix: restore ClientCard.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,14 +3,15 @@
 
 // import WSConsoleLogger from "@/components/WSConsoleLogger";
 import WorkoutBoard from "@/components/WorkoutBoard";
+import { AthledMinicard } from "@/components/AthledMinicard";
 
 export default function Home() {
     return (
         <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
             {/* <WSConsoleLogger /> */}
             <main className="flex min-h-screen w-full max-w-3xl flex-col items-center py-32 my-2 px-4 bg-white dark:bg-black sm:items-start">
-                <WorkoutBoard />
-                {/* <AthledMinicard /> */}
+                {/* <WorkoutBoard /> */}
+                <AthledMinicard />
             </main>
         </div>
     );
---
+++ frontend/components/ClientCard.tsx
@@ -0,0 +1,46 @@
+"use client";
+// components/ClientCard.tsx
+
+import { useClientsStore } from "@/store/clients";
+
+export function ClientCard({ clientId }: { clientId: string }) {
+    const client = useClientsStore((s) => s.clients[clientId]);
+
+    if (!client) return <div className="text-sm">Clients will be here</div>;
+
+    const hasErrors = client.currentErrors.length > 0;
+
+    return (
+        <div className="rounded-xl border border-gray-300 p-4 w-full max-w-[360px] bg-white shadow-lg my-1">
+            <h2 className="text-lg font-semibold"> Cliente {clientId}</h2>
+
+            <div className="flex justify-between text-sm mt-2">
+                {/* IZQUIERDA — ERRORES */}
+                <div className="flex-1">
+                    <p
+                        className={`font-medium ${
+                            hasErrors ? "text-red-600" : "text-green-600"
+                        }`}
+                    >
+                        {hasErrors
+                            ? "⚠️ Errores actuales"
+                            : "✅ Técnica correcta"}
+                    </p>
+
+                    {hasErrors && (
+                        <ul className="list-disc list-inside text-sm mt-1">
+                            {client.currentErrors.map((err, i) => (
+                                <li key={i}>{err}</li>
+                            ))}
+                        </ul>
+                    )}
+                </div>
+                {/* DERECHA — ESTADO */}
+                <div className="text-right min-w-[100px]">
+                    <p>Reps: {client.reps}</p>
+                    <p>Ejercicio: {client.exercise}</p>
+                </div>
+            </div>
+        </div>
+    );
+}
